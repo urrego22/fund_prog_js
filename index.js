5bfe50ce3c67472514cd44e61cde7d150aeffd7b
Sara Garcia <urregosara984@gmail.com>
2025-08-29 14:06:59 -0500

    agregar casos de prueba para calculateTip

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ console.log("Proyecto Fundamentos de Programación con JS");
 
 function calculateTip(billAmout, tipPercentage) {
       return (billAmout, tipPercentage) / 100;
-}
+};
+
+console.log(calculateTip(100, 10));
+console.log(calculateTip(1524.33, 25));
+console.log(calculateTip(50, 0));
+
+
